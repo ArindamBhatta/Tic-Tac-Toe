3a90bf78ccf66f456700de8485b143402e626ab9
Arindam Bhattacharyya <arindambhattacharyya.ab@gmail.com>
2024-07-05 00:42:44 +0530

    added all functionality

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import * as readlineSync from "readline-sync"
 import { LaunchGame } from "./LaunchGame"
 
-const numPlayers = parseInt(
-  readlineSync.question("Enter number of players (1 or 2) \n")
-)
+let numPlayers: number
+
+do {
+  numPlayers = parseInt(
+    readlineSync.question(`Enter the number of players (1 or 2)\n`)
+  )
+  if (numPlayers !== 1 && numPlayers !== 2) {
+    console.error("Number of players must be 1 or 2")
+  }
+} while (numPlayers !== 1 && numPlayers !== 2)
+
 const game = new LaunchGame(numPlayers)
 game.startGame()
